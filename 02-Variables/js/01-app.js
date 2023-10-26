477a29640039c82b2704d60e5bb65054fbfa4d3d
Belem <belemnun18@gmail.com>
2023-10-26 11:19:47 -0700

    Cambios guardados de Variables 02

---
+++ 02-Variables/js/01-app.js
@@ -0,0 +1,38 @@
+// Inicializar una variable con un valor
+
+var producto = 'Monitor de 23 pulgadas';
+console.log(producto);
+
+// Las variables se pueden reasignar
+producto = 'Monitor de 19 pulgadas';
+console.log(producto);
+
+// Javascript es un lenguaje de tipo dinamico, No se especifica tipo de dato
+producto = 20;
+console.log(producto);
+
+// Se pueden inicializar sin valor y asignarlo después 
+var disponibles;
+disponible = true;
+
+disponible = false;
+
+// Inicializar multiples variables
+var categorias = 'Computadoras',
+    marca = 'Marca farmosa',
+    calificacion  = 5;
+
+// // Las variables no pueden iniciar con números
+//  var dias 99;
+//  var dias99;
+
+//  var 01_;
+//  var _01;
+//  var _usuario;
+//  var __usuario;
+
+// // Definir variables de manera adecuada
+
+// var nombreProductosCategoriaPrecio;
+// var nombre_producto_categoria_precio;
+// var nombre producto
