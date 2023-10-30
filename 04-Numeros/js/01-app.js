c90aed89c6c8e3e237290691aad82c953a265073
Belem <belemnun18@gmail.com>
2023-10-30 11:58:22 -0700

    Numeros en Java Script

---
+++ 04-Numeros/js/01-app.js
@@ -0,0 +1,17 @@
+
+// Creación de números
+
+// Comillas se convierte en número y con "" se convierte en un string automáticamente
+
+const numero1 = 30;
+const numero2 = 30;
+const numero3 = 20.20;
+const numero4 =.102030;
+const numero5 = -20;
+
+
+console.log(numero1);
+console.log(numero2);
+console.log(numero3);
+console.log(numero4);
+console.log(numero5);
