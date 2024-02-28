05185810dbe872de08b95ef117a1700b0a7d3e3a
Belem <belemnun18@gmail.com>
2024-02-28 13:50:18 -0700

    Objetos

---
+++ 07-Objetos/js/01-app.js
@@ -0,0 +1,16 @@
+// Crear objetos en JavaScript
+
+const nombre = "Monitor 20 Pulgadas";
+const precio = 300;
+const disponible = true;
+
+// Un objeto agrupa toda en una sola variable
+
+// Object Literal
+const producto = {
+    nombre: "Monitor 20 Pulgadas",
+    precio: 300,
+    disponible: true,
+}
+
+console.log(producto);
