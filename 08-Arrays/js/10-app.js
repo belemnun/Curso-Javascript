af657a04ea095a842b9a42a0fb5b93cf5c8c396a
Belem <belemnun18@gmail.com>
2024-03-19 09:37:19 -0700

    Arreglos .map y carrito.forEach

---
+++ 08-Arrays/js/10-app.js
@@ -0,0 +1,24 @@
+// .forEach para iterar un array
+const carrito = [
+    {nombre: "Monitor 27 Pulgadas", precio: 500 },
+    {nombre: "Televisión", precio: 100 },
+    {nombre: "Tablet", precio: 200 },
+    {nombre: "Audifonos", precio: 300 },
+    {nombre: "Teclado", precio: 400 },
+    {nombre: "Celular", precio: 700 },
+]
+
+const nuevoArreglo = carrito.map ( function(producto) {
+return ( `${producto.nombre} - precio: ${producto.precio }` );
+
+} ) 
+
+const nuevoArreglo2 = carrito.forEach ( function(producto) {
+    console.log( `${producto.nombre} - precio: ${producto.precio }` );
+    
+    } ) 
+    
+
+    console.log(nuevoArreglo);
+    console.log(nuevoArreglo2);
+
